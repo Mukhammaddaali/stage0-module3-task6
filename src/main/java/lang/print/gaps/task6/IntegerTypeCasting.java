b58fbe29ff0d0bd449a4bcf0758a8c1baead5428
Muxammadali <115696592+Mukhammaddaali@users.noreply.github.com>
2023-02-13 14:24:26 +0500

    done

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first=1000L;
-        int seccond=(int) first;
-        System.out.println(first+" "+ seccond);
+        int second=(int) first;
+        System.out.println(first+"/n"+ second);
     }
 }
