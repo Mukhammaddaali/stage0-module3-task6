2ebdccb31e8701eec7cc1790233bdcc2a08236dc
Muxammadali <115696592+Mukhammaddaali@users.noreply.github.com>
2023-02-13 13:54:58 +0500

    done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task6;
 
+import java.util.Scanner;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        System.out.println(Math.round(numberToBeRounded));
     }
+}
+public static void main(String[] args){
+    float floatNumber;
+    Scanner input=new Scanner(System.in);
+    floatNumber=input.nextFloat();
+    FloatTypeCasting(floatNumber);
 }
